10bfe83be8e193a24b359f7a774a23bc0c8aeab9
Bruno Wilkinson <bruno.wilkinson@gmail.com>
2020-12-06 20:36:22 +0000

    feat: improve collision

--- Enemy.cs
+++ Enemy.cs
@@ -5,4 +5,12 @@ public class Enemy : Area2D
 {
     [Signal]
     public delegate void Hit();
+
+    [Signal]
+    public delegate void CollideWall();
+
+    public override void _Ready()
+    {
+        
+    }
 }
--- Wave.cs
+++ Wave.cs
@@ -1,13 +1,24 @@
 using Godot;
 using System;
 
-public class Wave : Area2D
+public class Wave : Node2D
 {
     [Export]
     public float speed = 40;
 
     [Export]
     public PackedScene enemyScene;
+    [Export]
+    public int row = 5;
+    [Export]
+    public int column = 8;
+
+    [Export]
+    public int rowOffset = 192;
+    [Export]
+    public int columnOffset = 128;
+    [Export]
+    public int gap = 16;
 
     enum AIState
     {
@@ -21,24 +32,22 @@ public class Wave : Area2D
     private AIState currentState;
     private Vector2 velocity = Vector2.Zero;
 
-    public void OnCollideRight()
+    public void OnCollideWall(Area2D area)
     {
         GD.Print("COLLIDE WITH RIGHT WALL");
-        currentState = AIState.MOVE_LEFT;
-        velocity = new Vector2(0, 1);
-    }
-
-     public void OnCollideLeft()
-    {
-        GD.Print("COLLIDE WITH LEFT WALL");
-        currentState = AIState.MOVE_RIGHT;
+        GD.Print(area.Name);
+        if (area.Name == "Right") {
+            currentState = AIState.MOVE_LEFT;
+        } else if (area.Name == "Left") {
+            currentState = AIState.MOVE_RIGHT;
+        }
         velocity = new Vector2(0, 1);
     }
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        GenerateEnemies(9, 5, 128, 192);
+        GenerateEnemies(row, column, rowOffset, columnOffset);
         currentState = AIState.MOVE_RIGHT;
         // animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
     }
@@ -51,31 +60,39 @@ public class Wave : Area2D
 
     private void Movement(float delta)
     {
-        if (currentState == AIState.MOVE_RIGHT) {
+        if (currentState == AIState.MOVE_RIGHT)
+        {
             velocity.x += 1;
-        } else {
+        }
+        else
+        {
             velocity.x -= 1;
         }
 
         Position += velocity.Normalized() * speed * delta;
     }
 
-    private void GenerateEnemies(int x, int y, int xOffset, int yOffset) {
-        int waveNumber = x * y;
+    private void GenerateEnemies(int rows, int columns, int yOffset, int xOffset)
+    {
         Vector2 screenSize = GetViewportRect().Size;
         Vector2 waveSize = new Vector2(screenSize.x - xOffset, screenSize.y - yOffset);
-        Enemy child = (Enemy)enemyScene.Instance();
+        int counter = 0;
 
-        for(int i = 0; waveNumber > i; i++) {
-            child.Name = "Enemy" + i;
-            AddChild(child);
-        }
-
-        for(int rows = 0; y > rows; rows++) {
-            for(int columns = 0; x > columns; columns++) {
-                GD.Print("X: ", rows, "Y: ", columns);
+        for (int y = 1; rows >= y; y++)
+        {
+            for (int x = 1; columns >= x; x++)
+            {
+                string enemyName = "Enemy";
+                AddChild((Enemy)enemyScene.Instance(), true);
+                counter++;
+                if (counter > 1) {
+                    enemyName += counter;
+                }
+                Area2D enemyNode = GetNode<Area2D>(enemyName);
+                enemyNode.Connect("area_entered", this, nameof(OnCollideWall));
+                enemyNode.Position = new Vector2(gap * x, gap * y);
+                GD.Print(enemyNode.Position);
             }
         }
-        
     }
 }

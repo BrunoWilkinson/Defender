4b9ff85b5ed8df54ddef91cc78c6274e9fc27d50
Bruno Wilkinson <bruno.wilkinson@gmail.com>
2020-12-17 09:12:33 +0000

    feat(Enemy): ground work for shooting

--- Scenes/Enemy/Enemy.cs
+++ Scenes/Enemy/Enemy.cs
@@ -5,10 +5,9 @@ public class Enemy : Area2D
 {
     [Signal]
     public delegate void onDestroy();
-    public override void _Ready()
-    {
-        Connect("area_entered", this, nameof(onHit));
-    }
+    [Signal]
+    delegate void onShoot(PackedScene bullet, Vector2 location);
+    private PackedScene _missile = GD.Load<PackedScene>("res://Scenes/Missile/Missile.tscn");
     public void onHit(Area2D area)
     {
         if (area.GetType().ToString() == "Missile")
@@ -17,5 +16,15 @@ public class Enemy : Area2D
             area.QueueFree();
             EmitSignal(nameof(onDestroy));
         }
+    }
+    public void shoot()
+    {
+        GD.Print(Position.y);
+        EmitSignal(nameof(onShoot), _missile, Position);
+    }
+    public override void _Ready()
+    {
+        Connect("area_entered", this, nameof(onHit));
+        GetNode<Timer>("RateOfFire").Connect("timeout", this, nameof(shoot));
     }
 }
--- Scenes/Main.cs
+++ Scenes/Main.cs
@@ -6,6 +6,13 @@ public class Main : Node2D
     public override void _Ready()
     {
         GetNode<Area2D>("Player").Connect("PressShoot", this, nameof(OnPlayerShoot));
+        foreach (Node child in GetNode<Node2D>("Wave").GetChildren())
+        {
+            if (child.GetType().ToString() == "Enemy")
+            {
+                child.Connect("onShoot", this, nameof(OnEnemyShoot));
+            }
+        }
     }
 
     public void OnPlayerShoot(PackedScene missile, Vector2 location)
@@ -16,4 +23,11 @@ public class Main : Node2D
         missileInstance.Velocity.y = -1;
     }
 
+    public void OnEnemyShoot(PackedScene missile, Vector2 location)
+    {
+        Missile missileInstance = (Missile)missile.Instance();
+        AddChild(missileInstance);
+        missileInstance.Position = location;
+        missileInstance.Velocity.y = 1;
+    }
 }

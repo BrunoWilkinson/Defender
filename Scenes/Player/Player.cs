75a835c4c3a5cfae4d24528113dcae8fc2e5ad88
Bruno Wilkinson <bruno.wilkinson@gmail.com>
2020-12-17 22:09:01 +0000

    feat(Player): on hit hide

--- Scenes/Player/Player.cs
+++ Scenes/Player/Player.cs
@@ -3,41 +3,52 @@ using System;
 
 public class Player : Area2D
 {
-      [Signal]
-      delegate void PressShoot(PackedScene bullet, Vector2 location);
+    [Signal]
+    delegate void PressShoot(PackedScene bullet, Vector2 location);
 
-      [Export]
-      public float speed = 500;
+    [Export]
+    public float speed = 500;
 
-      [Export]
-      public float spriteSize = 32;
+    [Export]
+    public float spriteSize = 32;
 
-      [Export]
-      public float fireRate = 1;
+    [Export]
+    public float fireRate = 1;
 
-      Vector2 screenSize;
-      float currentTime;
+    Vector2 screenSize;
+    float currentTime;
 
-      private PackedScene _missile = GD.Load<PackedScene>("res://Scenes/Missile/Missile.tscn");
+    private PackedScene _missile = GD.Load<PackedScene>("res://Scenes/Missile/Missile.tscn");
 
-      public override void _Ready()
+    public void onHit(Area2D area)
     {
-            screenSize = GetViewport().Size;
+      String type = area.GetType().ToString();
+      if (type == "Enemy" || type == "Rock")
+      {
+        Hide();
+        area.QueueFree();
+      }
     }
 
-      public override void _Process(float delta)
+    public override void _Ready()
     {
-            Controls(delta);
-            Shoot(delta);
+        screenSize = GetViewport().Size;
+        Connect("area_entered", this, nameof(onHit));
     }
 
-      private void Controls(float delta)
+    public override void _Process(float delta)
     {
-            Vector2 velocity = Vector2.Zero;
+        Controls(delta);
+        Shoot(delta);
+    }
 
-            if (Input.IsActionPressed("move_right"))
+    private void Controls(float delta)
+    {
+        Vector2 velocity = Vector2.Zero;
+
+        if (Input.IsActionPressed("move_right"))
         {
-                  velocity.x += 1;
+            velocity.x += 1;
         }
         if (Input.IsActionPressed("move_left"))
         {
@@ -50,13 +61,13 @@ public class Player : Area2D
         );
     }
 
-      private void Shoot(float delta)
+    private void Shoot(float delta)
     {
-            currentTime += delta;
-            if (Input.IsActionPressed("shoot") && currentTime > fireRate)
+        currentTime += delta;
+        if (Input.IsActionPressed("shoot") && currentTime > fireRate)
         {
-                  currentTime = 0;
-                  EmitSignal(nameof(PressShoot), _missile, Position);
+            currentTime = 0;
+            EmitSignal(nameof(PressShoot), _missile, Position);
         }
-      }
+    }
 }

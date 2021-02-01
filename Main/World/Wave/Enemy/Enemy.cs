bfd2eff23f815233646b15befebc5156641c38ab
Bruno Wilkinson <bruno.wilkinson@gmail.com>
2021-02-01 08:42:05 +0000

    feat(Wave): add enemy art + animation

--- Main/World/Wave/Enemy/Enemy.cs
+++ Main/World/Wave/Enemy/Enemy.cs
@@ -7,12 +7,16 @@ public class Enemy : Area2D
     public delegate void OnDestroy();
     [Signal]
     delegate void OnShoot(PackedScene bullet, Vector2 location, Area2D enemy);
+
+    [Export]
+    public int enemyVersion = 1;
     private PackedScene _rock = GD.Load<PackedScene>("res://Main/World/Wave/Enemy/Rock/Rock.tscn");
 
     public override void _Ready()
     {
         Connect("area_entered", this, nameof(OnHit));
         GetNode<Timer>("FireRate").Connect("timeout", this, nameof(Shoot));
+        GetNode<AnimatedSprite>("AnimatedSprite").Play("enemy_" + enemyVersion);
     }
 
     public void OnHit(Area2D area)

072f29eb053dc83780e95b20a4e89b87575d23e7
Bruno Wilkinson <bruno.wilkinson@gmail.com>
2021-02-02 23:33:25 +0000

    feat(Enemy): death animation

--- Main/World/Wave/Enemy/Enemy.cs
+++ Main/World/Wave/Enemy/Enemy.cs
@@ -12,19 +12,32 @@ public class Enemy : Area2D
     public int enemyVersion = 1;
     private PackedScene _rock = GD.Load<PackedScene>("res://Main/World/Wave/Enemy/Rock/Rock.tscn");
 
+    private AnimatedSprite _anim;
+
     public override void _Ready()
     {
         Connect("area_entered", this, nameof(OnHit));
         GetNode<Timer>("FireRate").Connect("timeout", this, nameof(Shoot));
-        GetNode<AnimatedSprite>("AnimatedSprite").Play("enemy_" + enemyVersion);
+        _anim = GetNode<AnimatedSprite>("AnimatedSprite");
+        _anim.Play("enemy_" + enemyVersion);
+        _anim.Connect("animation_finished", this, nameof(OnAnimationFinished));
+    }
+
+    public void OnAnimationFinished()
+    {
+        if (_anim.Animation == "death")
+        {
+            QueueFree();
+        }
     }
 
     public void OnHit(Area2D area)
     {
         if (area is Missile)
         {
-            QueueFree();
             area.QueueFree();
+            _anim.Play("death");
+            GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred("disabled", true);
             EmitSignal(nameof(OnDestroy));
         }
     }

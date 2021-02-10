30f1c67eef7f80c15e5dbcb0fd62ea875ec183c4
Bruno Wilkinson <bruno.wilkinson@gmail.com>
2021-02-10 09:08:21 +0000

    feat(GUI): exit game button

--- Main/GUI/Menu/Menu.cs
+++ Main/GUI/Menu/Menu.cs
@@ -9,14 +9,23 @@ public class Menu : Control
 
     private Sprite _speakerOff;
 
+    private Button _exitButton;
+
     private bool _mute = false;
 
     public override void _Ready()
     {
         _soundButton = GetNode<TextureButton>("SoundButton");
+        _exitButton = GetNode<Button>("ExitGame");
+        _exitButton.Connect("pressed", this, nameof(exitGame));
         _soundButton.Connect("pressed", this, nameof(changeSpeaker));
         _speaker = GetNode<Sprite>("Speaker");
         _speakerOff = GetNode<Sprite>("SpeakerOff");
+    }
+
+    public void exitGame()
+    {
+        GetTree().Quit();
     }
 
     public void changeSpeaker()
